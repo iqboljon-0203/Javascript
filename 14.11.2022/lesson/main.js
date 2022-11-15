d1a144f2788837c120f20791860917f910b65f3e
Iqboljon <iqboljon2003.yuldashev@gmail.com>
2022-11-16 00:26:04 +0500

    if,else%%nullish

---
+++ 14.11.2022/lesson/main.js
@@ -0,0 +1,53 @@
+////shart operator;
+///// truthy,falsy
+// if(true){
+//     console.log('salom');
+// }
+// else if(true){
+//    console.log("xayr");
+// }
+// else{
+//     console.log("Bye");
+// }
+// let age= +prompt("Yoshingizni kiriting","");
+// if(age<16 && age>0){
+//     console.log("siz pasport olishiz mumkin emas!");
+// }
+// else if(age>=16 && age<26){
+//     console.log("Pasportingizni almashtirmagansiz");
+// }
+// else{
+//     console.log("siz pasportingizni,yangilagansiz!");
+// }
+//////
+// if(age>16){
+//     console.log("siz pasport olishiz mumkin!");
+// }
+// if(age<=16){
+//     console.log("siz pasport ololmaysiz,chunki yoshiz yetmagan!");
+// }
+// if(age===26){
+//     console.log("Pasportingizni 1marta almashtirgansiz");
+// }
+
+///truthy,falsy
+// falsy->0,'',null,undefined,false,-0;
+// if(false) console.log("xayr");
+// else console.log("hello");
+// if(2){
+//     console.log("salom");
+// }
+//// Ternary operator ? :
+// let age = 12;
+// age > 16
+//   ? console.log("Siz olasiz pasport")
+//   : age > 10
+//   ? console.log("siz ozgina bor")
+//   : console.log("Ololmaysiz");
+///Nullish ?? -> ||;
+// console.log(false||'a');
+// || =>'',0,undefined,null,false,-0,NaN;
+/// ??=>undefined,null => fasle deb qabul qiladi;
+// console.log( null ?? "salom");
+// console.log( undefined ?? "salom");
+
