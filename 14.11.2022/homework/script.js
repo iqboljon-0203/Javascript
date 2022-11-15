5f301761c2af47755c35ac57052669181565a921
Iqboljon <iqboljon2003.yuldashev@gmail.com>
2022-11-16 00:24:18 +0500

    if,else%%nullish

---
+++ 14.11.2022/homework/script.js
@@ -0,0 +1,39 @@
+/// home work
+// if("0"){
+//     alert('Hello');
+// // }=>alertda Hello chiqadi chunki '0'-truthy
+// #2
+// let nameJs =prompt("What ish official name of Javascript","");
+// if(nameJs==="ESMAScript"){
+//     console.log("Right!");
+// }
+// else{
+//     console.log("You don't know? ESMAScript");
+// }
+// #3
+// let value=-8;
+// if(value>0){
+//     console.log(1);
+// }else if(value===0){
+//     console.log(0);
+// }
+// else{
+//     console.log(-1);
+// }
+// #4
+// let result;
+// let a=3;
+// let b=2;
+// (a+b<4)? result="Below" : result ="Over";
+// console.log(result);
+// #5
+// let message;
+// let login = "Director";
+// login == "Employee"
+//   ? (message = "Hello")
+//   : login == "Director"
+//   ? (message = "Greetings")
+//   : login == ""
+//   ? (message = "No login")
+//   : (message = "");
+//   console.log(message);
