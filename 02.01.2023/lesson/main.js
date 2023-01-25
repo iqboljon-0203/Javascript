8c3d522f36de6cba5809d6bc87560d3b7dc7c56f
Iqboljon <iqboljon2003.yuldashev@gmail.com>
2023-01-25 11:30:30 +0500

    DOM 6 Selection vs range

---
+++ 02.01.2023/lesson/main.js
@@ -0,0 +1,25 @@
+////DOM 6 Selection vs range
+// let range=new Range();
+// console.log(text);
+// range.setStart(text[0].firstChild,0);
+// range.setEnd(text[0].firstChild,5);
+// window.getSelection().addRange(range)
+// // range.deleteContents();
+// // let txt=range.extractContents();
+// // console.log(txt.textContent);
+// // let elm=range.cloneContents();
+// // console.log(elm.textContent);
+// // #insert
+// let span=document.createElement("span");
+// span.textContent="Hey salom";
+// span.style.color="red";
+// span.style.textDecoration="underline"
+// // range.insertNode(span)
+// // #surround
+// range.surroundContents(span)
+
+// range.startContainer();
+
+// #selection
+
+
